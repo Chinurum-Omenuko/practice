2dfba9ac426e37e47581370c53a13075a17ed407
Chinurum-Omenuko <omen0003@algonquinlive.com>
2023-07-11 05:50:38 -0400

    adds return statement

--- counter/counterapp/src/App.tsx
+++ counter/counterapp/src/App.tsx
@@ -1,6 +1,17 @@
 import './App.css'
+import Button from './components/button/button'
 
 function App() {
+    return(
+    <div className="counter-div">
+        <h1>1</h1>
+        <div className="buttons">
+            <Button sign="-"/>
+            <Button sign="+"/>
+        </div>
+        
+    </div>
+    )
 
 }
 
