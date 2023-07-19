debba4544ad8421a1099ef0ec3d699295a1bd156
Chinurum-Omenuko <omen0003@algonquinlive.com>
2023-07-19 16:20:15 -0400

    adds navscript that contains onclick open method

---
+++ navbar/navscript.js
@@ -0,0 +1,14 @@
+var hamburger = document.querySelector(".burger")
+var navbar = document.querySelector("nav")
+
+console.log("queries selected")
+
+
+hamburger.addEventListener("click", () => {
+
+    var height = navbar.style.height
+
+    
+
+    
+})
