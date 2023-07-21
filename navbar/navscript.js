adbbde5923be34ffba2529f1b49127637e9fdf42
Chinurum-Omenuko <omen0003@algonquinlive.com>
2023-07-21 19:21:40 -0400

    makes navmenu by changing style attribute

--- navbar/navscript.js
+++ navbar/navscript.js
@@ -1,14 +1,31 @@
-var hamburger = document.querySelector(".burger")
-var navbar = document.querySelector("nav")
+var hamburger = document.getElementById("burger")
+var navmenu = document.querySelector(".nav-menu")
+
+const options = {
+    show : 'display: block',
+    hide : 'display: none'
+}
+
 
 console.log("queries selected")
 
 
-hamburger.addEventListener("click", () => {
 
-    var height = navbar.style.height
-
+function change(){
+    let style = navmenu.getAttribute('style')
+    console.log(style)
+    
+    if(style == options.hide || null){
+        navmenu.setAttribute('style', `${options.show}`)
+        console.log(`style changed to: ${style}`)
+    }
+    else{
+        navmenu.setAttribute('style', `${options.hide}`)
+        console.log(`style changed to: ${style}`)
+    }
     
 
     
-})
+}
+
+
